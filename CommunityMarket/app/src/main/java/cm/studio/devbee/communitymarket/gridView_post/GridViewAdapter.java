63b0e4fcc9a60671fd65a7477fef1da1df21403c
ndemeYvan <sevenacademy@r4.local>
2019-03-04 19:21:21 +0100

    vendeur activity effectif

--- CommunityMarket/app/src/main/java/cm/studio/devbee/communitymarket/gridView_post/GridViewAdapter.java
+++ CommunityMarket/app/src/main/java/cm/studio/devbee/communitymarket/gridView_post/GridViewAdapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
@@ -34,7 +35,11 @@ public class GridViewAdapter extends RecyclerView.Adapter<GridViewAdapter.ViewHo
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
     String produit_image =modelGridViewList.get(i).getImage_du_produit();
+    String nom=modelGridViewList.get(i).getNom_du_produit();
+    String prix_produit=modelGridViewList.get(i).getPrix_du_produit();
+    viewHolder.prix_produit(prix_produit);
     viewHolder.image_produit(produit_image);
+    viewHolder.nom_produit(nom);
     }
 
     @Override
@@ -44,13 +49,23 @@ public class GridViewAdapter extends RecyclerView.Adapter<GridViewAdapter.ViewHo
 
     public class ViewHolder extends RecyclerView.ViewHolder{
         ImageView produit;
+        TextView post_titre_produit_description;
+        TextView prix_post;
 
        public ViewHolder(@NonNull View itemView) {
            super(itemView);
            produit=itemView.findViewById(R.id.post_image);
+           post_titre_produit_description=itemView.findViewById(R.id.post_titre_produit_description);
+           prix_post=itemView.findViewById(R.id.prix_post);
        }
        public void image_produit(String image){
            Picasso.with(context).load(image).into (produit );
        }
+        public void nom_produit(String nom){
+            post_titre_produit_description.setText(nom);
+        }
+        public void prix_produit(String prix){
+            prix_post.setText(prix);
+        }
    }
 }
