6a4506d22a42197011956a13b0f20933625a9ecf
nemeyvan <ndemeyvan@gmail.com>
2019-03-19 17:47:22 +0100

    bug de user general resolu

--- CommunityMarket/app/src/main/java/cm/studio/devbee/communitymarket/search/SearchActivity.java
+++ CommunityMarket/app/src/main/java/cm/studio/devbee/communitymarket/search/SearchActivity.java
@@ -4,17 +4,21 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import cm.studio.devbee.communitymarket.R;
+import cm.studio.devbee.communitymarket.utilsforsearch.SearchModel;
 
 public class SearchActivity extends AppCompatActivity {
     private EditText search_edit_text;
@@ -25,11 +29,11 @@ public class SearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
-        firebaseFirestore=FirebaseFirestore.getInstance ();
         setContentView ( R.layout.activity_search );
         search_edit_text=findViewById ( R.id.search_edit_text );
         search_buton=findViewById ( R.id.search_button );
         search_recyclerview=findViewById ( R.id.search_recyclerview );
+
         search_buton.setOnClickListener ( new View.OnClickListener () {
             @Override
             public void onClick(View v) {
@@ -39,7 +43,13 @@ public class SearchActivity extends AppCompatActivity {
     }
     public void firebaseSarch(){
 
+
     }
 
+    public class categorieSearchHolder extends RecyclerView.ViewHolder{
 
+        public categorieSearchHolder(@NonNull View itemView) {
+            super ( itemView );
+        }
+    }
 }
